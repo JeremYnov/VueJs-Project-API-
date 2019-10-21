dbb90552d808700b0a755d6a7cbcc2b8aedb4364
JeremYnov <jeremy.richard@ynov.com>
2019-10-21 12:33:39 +0200

    add modif

--- pokedex.php
+++ pokedex.php
@@ -17,73 +17,77 @@
                     <button class="click">A->Z</button>
                     <button class="click">Z->A</button>
                     <button class="click">1-></button>
-                    <button class="click"><-1</button> 
-                    <div v-for="(todo, index) in typefilter">
-                <div v-on:click="typefilter(index)" class="type click" v-bind:class="typefilter[index]"> {{ typefilter[index] }} </div>
-                </div>
-                
-                </div>
-        </div>
-        <div class="poke-container">
+                    <button class="click">1</button>
 
-            <div v-if="FilterCheck == false">
-                <div class="poke-wrapper">
-                    <div v-on:click="url(infoPokemon[index].id)" v-for="(todo, index) in infoPokemon" class="pokemon">
+                    <div class="poke-filter-type">
+                        <div v-for="(todo, index) in typefilter" class="type-container">
+                            <div v-on:click="typefilter(index)" class="type click" v-bind:class="typefilter[index]"> {{ typefilter[index] }} </div>
+                        </div>
+                    </div>
 
-                        <a v-bind:href="urlPokemon">
-                            <div class="poke-image">
-                                <img :src="infoPokemon[index].image" alt="">
-                            </div>
-                            <div class="poke-infos">
-                                <div class="poke-id">
-                                    <p v-if="index < 9">N°00{{ index + 1}}</p>
-                                    <p v-else-if="index < 99">N°0{{ index + 1}}</p>
-                                    <p v-else>N°{{ index + 1}}</p>
+
+                </div>
+            </div>
+            <div class="poke-container">
+
+                <div v-if="FilterCheck == false">
+                    <div class="poke-wrapper">
+                        <div v-on:click="url(infoPokemon[index].id)" v-for="(todo, index) in infoPokemon" class="pokemon">
+
+                            <a v-bind:href="urlPokemon">
+                                <div class="poke-image">
+                                    <img :src="infoPokemon[index].image" alt="">
                                 </div>
-                                <div class="poke-name">
-                                    <p>{{ namePokemon[index] }}</p>
+                                <div class="poke-infos">
+                                    <div class="poke-id">
+                                        <p v-if="index < 9">N°00{{ index + 1}}</p>
+                                        <p v-else-if="index < 99">N°0{{ index + 1}}</p>
+                                        <p v-else>N°{{ index + 1}}</p>
+                                    </div>
+                                    <div class="poke-name">
+                                        <p>{{ namePokemon[index] }}</p>
+                                    </div>
+                                    <div class="poke-type">
+                                        <div v-for="(todo, value) in typefilter">
+                                            <div v-if=" infoPokemon[index].type1 == typefilter[value]" class="type click" v-bind:class="typefilter[value]"> {{ infoPokemon[index].type1 }} </div>
+                                            <div v-if=" infoPokemon[index].type2 == typefilter[value]" class="type click" v-bind:class="typefilter[value]"> {{ infoPokemon[index].type2 }} </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="poke-type">
-                                <div v-for="(todo, value) in typefilter">
-                                    <div v-if=" infoPokemon[index].type1 == typefilter[value]" class="type click" v-bind:class="typefilter[value]"> {{ infoPokemon[index].type1 }} </div>
-                                    <div v-if=" infoPokemon[index].type2 == typefilter[value]" class="type click" v-bind:class="typefilter[value]"> {{ infoPokemon[index].type2 }} </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div v-if="FilterCheck == true">
+                    <div class="poke-wrapper">
+                        <div v-on:click="url(filterName[index].id)" v-for="(todo, index) in filterName" class="pokemon">
+                            <a v-bind:href="urlPokemon">
+                                <div class="poke-image">
+                                    <img :src="filterName[index].image" alt="">
                                 </div>
+                                <div class="poke-infos">
+                                    <div class="poke-id">
+                                        <p v-if="filterName[index].id < 9">N°00{{ filterName[index].id }}</p>
+                                        <p v-else-if="filterName[index].id < 99">N°0{{ filterName[index].id }}</p>
+                                        <p v-else>N°{{ filterName[index].id }}</p>
+                                    </div>
+                                    <div class="poke-name">
+                                        <p>{{ filterName[index].name }}</p>
+                                    </div>
+                                    <div class="poke-type">
+                                        <div v-for="(todo, value) in typefilter">
+                                            <div v-if=" infoPokemon[index].type1 == typefilter[value]" class="type click" v-bind:class="typefilter[value]"> {{ infoPokemon[index].type1 }} </div>
+                                            <div v-if=" infoPokemon[index].type2 == typefilter[value]" class="type click" v-bind:class="typefilter[value]"> {{ infoPokemon[index].type2 }} </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </a>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div v-if="FilterCheck == true">
-                <div class="poke-wrapper">
-                    <div v-on:click="url(filterName[index].id)" v-for="(todo, index) in filterName" class="pokemon">
-                        <a v-bind:href="urlPokemon">
-                            <div class="poke-image">
-                                <img :src="filterName[index].image" alt="">
-                            </div>
-                            <div class="poke-infos">
-                                <div class="poke-id">
-                                    <p v-if="filterName[index].id < 9">N°00{{ filterName[index].id }}</p>
-                                    <p v-else-if="filterName[index].id < 99">N°0{{ filterName[index].id }}</p>
-                                    <p v-else>N°{{ filterName[index].id }}</p>
-                                </div>
-                                <div class="poke-name">
-                                    <p>{{ filterName[index].name }}</p>
-                                </div>
-                                <div class="poke-type">
-                                <div v-for="(todo, value) in typefilter">
-                                    <div v-if=" infoPokemon[index].type1 == typefilter[value]" class="type click" v-bind:class="typefilter[value]"> {{ infoPokemon[index].type1 }} </div>
-                                    <div v-if=" infoPokemon[index].type2 == typefilter[value]" class="type click" v-bind:class="typefilter[value]"> {{ infoPokemon[index].type2 }} </div>
-                                </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
         </div>
-    </div>
 </section>
 
 <?php require_once("includes/footer.php") ?>
